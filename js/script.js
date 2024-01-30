970c8d8262f2844e0fa8a80f9467d25a8768dcf9
Alberto-Arrighetti <alberto.arrighetti1571@gmail.com>
2024-01-30 22:57:08 +0100

    aggiunta visualizzazione in pagina di js

--- js/script.js
+++ js/script.js
@@ -27,9 +27,16 @@ buttonEl.addEventListener(`click`,
         // sconti applicati
         if (error == false){
             if(ageEl.value < 18){
-                discountPrice = basePrice - basePrice * 0.2;
+                discountPrice = basePrice - basePrice * 0.2;          
+                // tariffe
+                document.getElementById("offers").innerHTML = "Biglietto ridotto"
             }else if(ageEl.value > 65){
                 discountPrice = basePrice - basePrice * 0.4;
+                // tariffe
+                document.getElementById("offers").innerHTML = "Biglietto over 65"
+            }else {
+                // tariffe
+                document.getElementById("offers").innerHTML = "Biglietto Standard"
             }
         }
         
@@ -37,12 +44,23 @@ buttonEl.addEventListener(`click`,
 
         // risultato in pagina
         if(! error){
-            document.getElementById("result").innerHTML = `
-            Il prezzo del tuo biglietto è: € ${discountPrice.toFixed(2)}`
+            // nome passeggero 
+            document.getElementById("fullName").innerHTML = fullNameEl.value;
+
+            // carrozza assegnata
+            const trainCarriage = Math.floor( Math.random() * 10 + 1);
+            document.getElementById("train-carriage").innerHTML = trainCarriage;
+
+            // codice biglietto
+            const ticketCode = Math.floor( Math.random() * 99999 + 1);
+            document.getElementById("code").innerHTML = ticketCode;
+
+            // prezzo finale 
+            document.getElementById("cost").innerHTML = discountPrice.toFixed(2) + " €";
         }
 
         // log
-        console.log(discountPrice); 
+        // console.log(discountPrice); 
     }
 )
 
